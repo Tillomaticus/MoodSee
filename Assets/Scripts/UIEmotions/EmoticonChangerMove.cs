8ef52a65a78fd586fa003637892e69957a69a986
Rodrigo Silvetti Murillo <56391367+rodrex666@users.noreply.github.com>
2025-08-18 23:35:07 +0200

    emotion changer

--- Assets/Scripts/UIEmotions/EmoticonChangerMove.cs
+++ Assets/Scripts/UIEmotions/EmoticonChangerMove.cs
@@ -2,19 +2,16 @@ using UnityEngine;
 
 public class EmoticonChangerMove : MonoBehaviour
 {
-    public class MenuMover : MonoBehaviour
+    [SerializeField] private GameObject centerEyeAnchor; // Reference to the center eye anchor in the CameraRig
+    [SerializeField] private GameObject gui; // Reference to the GUI GameObject
+
+    [SerializeField] private float X = 0;
+    [SerializeField] private float Y = 0;
+
+    // Update is called once per frame
+    void Update()
     {
-        [SerializeField] private GameObject centerEyeAnchor; // Reference to the center eye anchor in the CameraRig
-        [SerializeField] private GameObject gui; // Reference to the GUI GameObject
-
-        [SerializeField] private float X = 0;
-        [SerializeField] private float Y = 0;
-
-        // Update is called once per frame
-        void Update()
-        {
-            gui.transform.position = centerEyeAnchor.transform.position + centerEyeAnchor.transform.forward * 0.6f;
-            gui.transform.rotation = Quaternion.LookRotation(gui.transform.position - centerEyeAnchor.transform.position);
-        }
+        gui.transform.position = centerEyeAnchor.transform.position + centerEyeAnchor.transform.forward * 0.6f;
+        gui.transform.rotation = Quaternion.LookRotation(gui.transform.position - centerEyeAnchor.transform.position);
     }
 }
--- Assets/__Detection/UnitySample/UpdatePositionScript.cs
+++ Assets/__Detection/UnitySample/UpdatePositionScript.cs
@@ -4,11 +4,13 @@ public class UpdatePositionScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject EmoticonChanger;
+    [SerializeField] private GameObject centerEyeAnchor; // Reference to the center eye anchor in the CameraRig
 
     public void UpdatePosition(FaceDetectionResult result)
     {
         this.transform.position = result.worldPosition;
         EmoticonChanger.transform.position = result.worldPosition + new Vector3(0, (result.worldPosition.y+result.imageSize.y/2),0);
+        EmoticonChanger.transform.rotation = Quaternion.LookRotation(centerEyeAnchor.transform.position - EmoticonChanger.transform.position);
     }
 
 }
