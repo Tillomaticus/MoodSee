7c20a162741b7093d23b3686872050bc5c0ad063
Rodrigo Silvetti Murillo <56391367+rodrex666@users.noreply.github.com>
2025-08-18 22:53:44 +0200

    updated position script

--- Assets/__Detection/UnitySample/UpdatePositionScript.cs
+++ Assets/__Detection/UnitySample/UpdatePositionScript.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class UpdatePositionScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject EmoticonChanger;
 
     public void UpdatePosition(FaceDetectionResult result)
     {
         this.transform.position = result.worldPosition;
+        EmoticonChanger.transform.position = result.worldPosition + new Vector3(0, (result.worldPosition.y+result.imageSize.y/2),0);
     }
+
 }
